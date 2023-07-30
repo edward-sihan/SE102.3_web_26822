0c9fae33b3d43a9574c12a7537faef9bc9553e08
edward-sihan <hansajasihan2002@gmail.com>
2023-07-30 21:21:25 +0530

    web

---
+++ Web Tutorials/javascript/tute6.js
@@ -0,0 +1,5 @@
+//document.write("Hello world <br>");
+//document.write("Hello world");
+//alert("This is a alert box");
+//confirm("Are you sure you want to log out");
+//prompt("Enter your name");
